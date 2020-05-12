39d96a40cdf182af9c0c8fde57cc2f6d09a7289c
gzeru <gzeru@hotmail.de>
2020-05-12 22:20:10 +0200

    changed the html for the site

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+#from . import views
+#from django.urls import path
+
+#urlpatterns = [
+#    path('', views.PostList.as_view(), name='home'),
+#    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+#]
+
+from	django.urls	import	path
+from	.	import	views
+
+urlpatterns	=	[
+	path('',	views.post_list,	name='post_list'), ]
